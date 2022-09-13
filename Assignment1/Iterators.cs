7bf1575532969e02cb5a0512e3185c5a11c97dbe
Magnus Brun Larsen <magnusbrunlarsen00@gmail.com>
2022-09-13 13:32:46 +0200

    Implement flatten again

--- Assignment1/Iterators.cs
+++ Assignment1/Iterators.cs
@@ -2,8 +2,15 @@ namespace Assignment1;
 
 public static class Iterators
 {
-    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items) {
-        return (IEnumerable<T>) new List<int>();
+    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items) 
+    {
+        foreach(var stream in items)
+        {
+            foreach(var item in stream)
+            {
+                yield return item;
+            }
+        }
     }
 
     public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate)
